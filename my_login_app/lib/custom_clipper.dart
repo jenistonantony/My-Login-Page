9bed266188305ee1ee7e020d4e94b903c3d96d74
antony jeniston <antonyjeniston8601@gmail.com>
2024-04-24 17:39:57 +0530

    first commit

---
+++ my_login_app/lib/custom_clipper.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CustomClipperWidget extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final path = Path();
+
+    path.lineTo(0, size.height * 0.8);
+    path.quadraticBezierTo(
+        size.width * 0.7, size.height, size.width, size.height * 0.90);
+    path.lineTo(size.width, 0);
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
